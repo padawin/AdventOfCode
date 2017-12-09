283df28100121fb8a3724db5f2d439217d9fc01b
Ghislain Rodrigues <git@ghislain-rodrigues.fr>
2017-12-09 10:56:30 +0000

    day 9 - part 2

--- day9.py
+++ day9.py
@@ -2,18 +2,8 @@ import sys
 
 
 def process_line(l):
-    score = 0
-    level = 0
-    for c in l:
-        if c == '{':
-            level += 1
-            score += level
-        elif c == '}':
-            level -= 1
-    return score
+    print(len(l) - 2)
+    return len(l) - 2
 
 
-for line in sys.stdin:
-    l = line.strip()
-    res = process_line(l)
-    print("For line {}, the score is: {}".format(l, res))
+print(sum(process_line(l.strip()) for l in sys.stdin))
