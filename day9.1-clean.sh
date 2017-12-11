4cc291a59b46adc1f73f0b94c633adeabcdc1f35
Ghislain Rodrigues <git@ghislain-rodrigues.fr>
2017-12-11 08:58:36 +0000

    day9 update

--- day9.1-clean.sh
+++ day9.1-clean.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 # use as:
 # cat datafile | ./day9.1-clean.sh > cleaneddatafile
-perl -p -e 's/!.//g' | egrep -o '<[^>]*>'
+perl -p -e 's/!.//g' | egrep -o '<[^>]*>' | wc | awk '{print $3 - $2 - $1 - $1}'
