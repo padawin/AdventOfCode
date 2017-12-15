717f7371275d6281cf8c50c19e3b8b5973dfb6aa
Ghislain Rodrigues <git@ghislain-rodrigues.fr>
2017-12-15 09:47:32 +0000

    Day 15 - code simplified

--- day15.c
+++ day15.c
@@ -4,17 +4,13 @@ int find_nb_common(int a, int b) {
     int factor_a = 16807;
     int factor_b = 48271;
     long mod = 2147483647;
-    long previous_power_a = 1;
-    long previous_power_b = 1;
+    long power_a = a;
+    long power_b = b;
     int nb = 0;
 	for (int i = 0; i < 40000000; ++i) {
-        long power_a = (previous_power_a % mod) * factor_a;
-        previous_power_a = power_a;
-        long power_b = (previous_power_b % mod) * factor_b;
-        previous_power_b = power_b;
-        long calc_a = (a * (power_a % mod)) % mod;
-        long calc_b = (b * (power_b % mod)) % mod;
-        if ((calc_a & 65535) == (calc_b & 65535)) {
+        power_a = ((power_a % mod) * factor_a) % mod;
+        power_b = ((power_b % mod) * factor_b) % mod;
+        if ((power_a & 65535) == (power_b & 65535)) {
             nb += 1;
 		}
 	}
