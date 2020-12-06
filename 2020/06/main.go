6a2c42cc489cc93a671921bb6f4a777af5db8d5a
Ghislain Rodrigues <git@ghislain-rodrigues.fr>
2020-12-06 11:44:20 +0100

    2020 - Day 6 - part 2

--- 2020/06/main.go
+++ 2020/06/main.go
@@ -25,6 +25,36 @@ func part1() {
 	fmt.Println(count)
 }
 
+func part2() {
+	answers := map[rune]int{}
+	count := 0
+	groupSize := 0
+	scanner := bufio.NewScanner(os.Stdin)
+	for scanner.Scan() {
+		line := scanner.Text()
+		if line == "" {
+			for _, c := range answers {
+				if c == groupSize {
+					count++
+				}
+			}
+			answers = map[rune]int{}
+			groupSize = 0
+			continue
+		}
+		for _, answer := range line {
+			answers[answer]++
+		}
+		groupSize++
+	}
+	for _, c := range answers {
+		if c == groupSize {
+			count++
+		}
+	}
+	fmt.Println(count)
+}
+
 func main() {
 	if len(os.Args) != 2 {
 		fmt.Println("Usage: main.go [1|2]")
@@ -32,5 +62,7 @@ func main() {
 	}
 	if os.Args[1] == "1" {
 		part1()
+	} else {
+		part2()
 	}
 }
