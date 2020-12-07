635fc64f6b2b40f3320d80171861a3feb7d73d24
Ghislain Rodrigues <git@ghislain-rodrigues.fr>
2020-12-07 12:47:24 +0100

    2020 - Day 7 - part 2

--- 2020/07/main.go
+++ 2020/07/main.go
@@ -5,6 +5,7 @@ import (
 	"fmt"
 	"os"
 	"regexp"
+	"strconv"
 )
 
 func processPossibleContainers(allContainers map[string]map[string]struct{}, possibleContainers map[string]struct{}, out map[string]struct{}) map[string]struct{} {
@@ -17,9 +18,10 @@ func processPossibleContainers(allContainers map[string]map[string]struct{}, pos
 	return out
 }
 
+var regexContainerBag = regexp.MustCompile(`^([a-z]+ [a-z]+) bags contain`)
+
 func part1() {
-	regexContainerBag := regexp.MustCompile(`^([a-z]+ [a-z]+) bags contain`)
-	regexContainedBags := regexp.MustCompile(`\d+ ([a-z]+ [a-z]+) bag`)
+	var regexContainedBags = regexp.MustCompile(`\d+ ([a-z]+ [a-z]+) bag`)
 	containedPossibilities := map[string]map[string]struct{}{}
 	scanner := bufio.NewScanner(os.Stdin)
 	for scanner.Scan() {
@@ -38,6 +40,36 @@ func part1() {
 	fmt.Println(len(res))
 }
 
+func processRequiredContent(prefix string, allContainers map[string]map[string]int, start string, multiplier int) int {
+	count := 0
+	//fmt.Printf("%s%s has to contain:\n", prefix, start)
+	for bag, nb := range allContainers[start] {
+		//fmt.Printf("%s%d %s (total: %d)\n", prefix, nb, bag, multiplier*nb)
+		count += multiplier * nb
+		count += processRequiredContent(fmt.Sprintf("\t%s", prefix), allContainers, bag, nb*multiplier)
+	}
+	return count
+}
+
+func part2() {
+	var regexContainedBags = regexp.MustCompile(`(\d+) ([a-z]+ [a-z]+) bag`)
+	containedPossibilities := map[string]map[string]int{}
+	scanner := bufio.NewScanner(os.Stdin)
+	for scanner.Scan() {
+		line := scanner.Text()
+		submatchContainer := regexContainerBag.FindStringSubmatch(line)
+		container := submatchContainer[1]
+		submatchesContained := regexContainedBags.FindAllStringSubmatch(line, -1)
+		containedPossibilities[container] = map[string]int{}
+		for _, submatch := range submatchesContained {
+			count, _ := strconv.Atoi(submatch[1])
+			containedPossibilities[container][submatch[2]] = count
+		}
+	}
+	res := processRequiredContent("", containedPossibilities, "shiny gold", 1)
+	fmt.Println(res)
+}
+
 func main() {
 	if len(os.Args) != 2 {
 		fmt.Println("Usage: main.go [1|2]")
@@ -45,5 +77,7 @@ func main() {
 	}
 	if os.Args[1] == "1" {
 		part1()
+	} else {
+		part2()
 	}
 }
