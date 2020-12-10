087e543cd4478188ca0c4bbd6c76132b613867fc
Ghislain Rodrigues <git@ghislain-rodrigues.fr>
2020-12-10 16:34:03 +0100

    2020 - Day 10 - part 2

--- 2020/10/main.go
+++ 2020/10/main.go
@@ -3,6 +3,7 @@ package main
 import (
 	"bufio"
 	"fmt"
+	"math"
 	"os"
 	"sort"
 	"strconv"
@@ -33,6 +34,43 @@ func part1() {
 	fmt.Println(countDiff1 * countDiff3)
 }
 
+func calculateMultiplier(numberConsecutive float64) float64 {
+	var multiplier float64
+	multiplier = 1
+	if numberConsecutive > 2 {
+		multiplier = math.Pow(2, numberConsecutive-2)
+		if numberConsecutive >= 5 {
+			multiplier -= math.Pow(2, numberConsecutive-5+1) - 1
+		}
+	}
+	return multiplier
+}
+
+func part2() {
+	scanner := bufio.NewScanner(os.Stdin)
+	adapters := []int{0}
+	for scanner.Scan() {
+		line := scanner.Text()
+		val, _ := strconv.Atoi(line)
+		adapters = append(adapters, val)
+	}
+	sort.Ints(adapters)
+	previousVal := 0
+	var numberConsecutive float64
+	combinations := 1.0
+	for _, adapter := range adapters {
+		if previousVal+1 == adapter {
+			numberConsecutive++
+		} else {
+			combinations *= calculateMultiplier(numberConsecutive)
+			numberConsecutive = 1
+		}
+		previousVal = adapter
+	}
+	combinations *= calculateMultiplier(numberConsecutive)
+	fmt.Println(combinations)
+}
+
 func main() {
 	if len(os.Args) != 2 {
 		fmt.Println("Usage: main.go [1|2]")
@@ -40,5 +78,7 @@ func main() {
 	}
 	if os.Args[1] == "1" {
 		part1()
+	} else {
+		part2()
 	}
 }
