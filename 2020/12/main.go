3dcc22601892dbf46e4a4fcac7c40bfbf78f566c
Ghislain Rodrigues <git@ghislain-rodrigues.fr>
2020-12-12 20:30:33 +0100

    2020 - Day 12 - part 2

--- 2020/12/main.go
+++ 2020/12/main.go
@@ -47,6 +47,44 @@ func part1() {
 	fmt.Println(int(math.Abs(float64(positionX)) + math.Abs(float64(positionY))))
 }
 
+func part2() {
+	scanner := bufio.NewScanner(os.Stdin)
+	positionX := 0
+	positionY := 0
+	positionWaypointX := 10
+	positionWaypointY := 1
+	for scanner.Scan() {
+		line := scanner.Text()
+		action := line[0]
+		value, _ := strconv.Atoi(line[1:])
+		if action == 'E' {
+			positionWaypointX += value
+		} else if action == 'S' {
+			positionWaypointY -= value
+		} else if action == 'W' {
+			positionWaypointX -= value
+		} else if action == 'N' {
+			positionWaypointY += value
+		} else if action == 'F' {
+			positionX += positionWaypointX * value
+			positionY += positionWaypointY * value
+		} else if action == 'R' {
+			for i := 1; i <= value/90; i++ {
+				tmp := positionWaypointX
+				positionWaypointX = positionWaypointY
+				positionWaypointY = -tmp
+			}
+		} else if action == 'L' {
+			for i := 1; i <= value/90; i++ {
+				tmp := positionWaypointX
+				positionWaypointX = -positionWaypointY
+				positionWaypointY = tmp
+			}
+		}
+	}
+	fmt.Println(int(math.Abs(float64(positionX)) + math.Abs(float64(positionY))))
+}
+
 func main() {
 	if len(os.Args) != 2 {
 		fmt.Println("Usage: main.go [1|2]")
@@ -54,5 +92,7 @@ func main() {
 	}
 	if os.Args[1] == "1" {
 		part1()
+	} else {
+		part2()
 	}
 }
